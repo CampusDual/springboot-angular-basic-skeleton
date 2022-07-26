9b4050607cac5a1d73ba32ca55462e821cfffb04
Millán Dasairas <82199419+millandasairas@users.noreply.github.com>
2022-07-26 14:39:46 +0200

    initial commit

---
+++ demo-backend/src/main/java/com/example/demo/entity/Contact.java
@@ -0,0 +1,66 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String surname1;
+	private String surname2;
+	private Integer phone;
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname1() {
+		return surname1;
+	}
+
+	public void setSurname1(String surname1) {
+		this.surname1 = surname1;
+	}
+
+	public String getSurname2() {
+		return surname2;
+	}
+
+	public void setSurname2(String surname2) {
+		this.surname2 = surname2;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
